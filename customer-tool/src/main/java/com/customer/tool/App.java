0b42af24b3ac15889389fa6a4d121de793b8e6bb
Harsha <sriharsha.jayanthi@initechglobal.com>
2019-07-26 16:03:21 +0530

    Initial commit !

---
+++ customer-tool/src/main/java/com/customer/tool/App.java
@@ -0,0 +1,13 @@
+package com.customer.tool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
